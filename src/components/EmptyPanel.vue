670fdf4ff39d5bd3f8b5d84bd19c5cff4baa1d9e
Cheng_DX <2337040896@qq.com>
2022-04-22 19:50:55 +0800

    feat: add empty img when there is no data or formulas

---
+++ src/components/EmptyPanel.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+}>()
+</script>
+
+<template>
+  <div hp-100 flex-center flex-col>
+    <img class="img" src="@/assets/open-box.png" />
+    <span class="text" mt-3px>{{ text ? text : 'No Results' }}</span>
+  </div>
+</template>
+
+<style scoped>
+.img {
+  width: 100px;
+  height: 100px;
+}
+.text {
+  color: #c48d69;
+}
+</style>
--- src/views/EditPanel.vue
+++ src/views/EditPanel.vue
@@ -9,6 +9,7 @@ import {
 } from '@/core'
 
 import AddPanel from '@/components/AddPanel.vue'
+import EmptyPanel from '../components/EmptyPanel.vue'
 
 initLocalStroage()
 const { displayedData, displayedFormulas } = initDisplayedItems()
@@ -42,7 +43,7 @@ function onInput() {
   <add-panel />
   <div flex hp-100>
     <div flex hp-100 wp-25 color-white flex-col items-center scroll-y>
-      <div mt-10px wp-95>
+      <div key="data" v-if="displayedData.length > 0" mt-10px wp-95>
         <div
           v-for="item of displayedData"
           :key="item.code"
@@ -62,18 +63,19 @@ function onInput() {
           <span wp-40 class="data-card-item">{{ item.name }}</span>
         </div>
       </div>
+      <empty-panel text="No data" key="data-empty" v-else />
     </div>
 
     <div flex flex-col items-center flex-grow m-10px r-10>
       <textarea @input="onInput" v-model="input" class="area" />
       <div>
-        <button @click="clear" btn bg-red hover:bg-red-500>Clear</button>
+        <button @click="clear" btn bg-red hover:bg-red-500 h-30px>Clear</button>
       </div>
       <textarea v-model="output" class="area" />
     </div>
 
     <div flex hp-100 wp-25 flex-col items-center scroll-y>
-      <div mt-10px wp-95>
+      <div key="formula" v-if="displayedFormulas.length > 0" mt-10px wp-95>
         <div
           v-for="item of displayedFormulas"
           flex
@@ -94,6 +96,7 @@ function onInput() {
           </span>
         </div>
       </div>
+      <empty-panel text="No Formulas" key="formula-empty" v-else />
     </div>
   </div>
 </template>
