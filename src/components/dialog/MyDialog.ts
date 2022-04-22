fd8e894a87c12d89c36a377a3a701787286aed2c
Cheng_DX <2337040896@qq.com>
2022-04-22 17:25:22 +0800

    feat: dialog component

---
+++ src/components/dialog/MyDialog.ts
@@ -0,0 +1,48 @@
+import { defineComponent, Fragment, h } from 'vue'
+import './dialog.css'
+
+export const MyDialog = defineComponent({
+  name: 'Dialog',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit, slots }) {
+    console.log(slots)
+    return () =>
+      h('div', { class: ['modal', props.modelValue ? '' : 'is-hidden'] }, [
+        h(
+          'div',
+          {
+            class: ['dialog', props.modelValue ? '' : 'is-hidden']
+          },
+          [
+            h('div', { class: 'dialog-header' }, [
+              h('div', null, [
+                h('span', { class: 'title' }, props.title),
+                h(Fragment, null, slots.header?.())
+              ]),
+              h(
+                'button',
+                {
+                  class: 'close-button',
+                  onClick: () => {
+                    emit('update:modelValue', false)
+                  }
+                },
+                '❌'
+              )
+            ]),
+            h('div', { class: 'dialog-body' }, slots.default?.())
+          ]
+        )
+      ])
+  }
+})
