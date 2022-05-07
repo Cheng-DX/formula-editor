e39627b799291f0d4445fe23216a049669837c39
Cheng_DX <2337040896@qq.com>
2022-05-07 20:14:02 +0800

    feat: add version name and link

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import {
   addPanelVisible,
   clearAllLocalStorage,
@@ -28,30 +28,37 @@ function switchUseTips() {
     formula: () => true,
   }
 }
+
+const version = ref({
+  name: 'padding...',
+  href: '',
+})
+async function fetchVersion() {
+  const data = await fetch('https://api.github.com/repos/Cheng-DX/formula-editor/releases/latest')
+  const json = await data.json()
+  version.value.name = json.name
+  version.value.href = json.html_url
+}
+fetchVersion()
+
 </script>
 
 <template>
   <header wp-100>
     <div h-30px p-10px flex justify-between items-center>
       <div flex items-center wp-70>
-        <a
-          icon-btn color-black h-7 w-7 i-carbon-logo-github href="https://github.com/Cheng-DX/formula-tool"
-          target="_blank" title="GitHub"
-        />
+        <a icon-btn color-black h-7 w-7 i-carbon-logo-github href="https://github.com/Cheng-DX/formula-tool"
+          target="_blank" title="GitHub" />
         <label for="uploadFile" position-relative>
           <button :class="buttonClass">Data</button>
-          <input
-            type="file" multiple accept=".json" position-absolute left-0 top-0 opacity-0 h-10 w-159px
-            cursor-pointer @change="e => handleImportFiles(e, 'INFO')"
-          >
+          <input type="file" multiple accept=".json" position-absolute left-0 top-0 opacity-0 h-10 w-159px
+            cursor-pointer @change="e => handleImportFiles(e, 'INFO')">
         </label>
 
         <label for="uploadFile" position-relative>
           <button :class="buttonClass">Formula</button>
-          <input
-            type="file" multiple accept=".json" position-absolute left-0 top-0 opacity-0 h-10 w-159px
-            cursor-pointer @change="e => handleImportFiles(e, 'FORMULA')"
-          >
+          <input type="file" multiple accept=".json" position-absolute left-0 top-0 opacity-0 h-10 w-159px
+            cursor-pointer @change="e => handleImportFiles(e, 'FORMULA')">
         </label>
         <button :class="buttonClass" @click="exportLocalStorage">
           Export
@@ -63,12 +70,16 @@ function switchUseTips() {
           Add
         </button>
       </div>
-      <button
-        :class="buttonClass" w-100px mr-10px btn bg-red hover:bg-red-500 style="max-width: 150px"
-        @click="clearAllLocalStorage"
-      >
-        Delete
-      </button>
+      <div flex items-center>
+        <button :class="buttonClass" w-100px mr-10px btn bg-red hover:bg-red-500 style="max-width: 150px"
+          @click="clearAllLocalStorage">
+          Delete
+        </button>
+        <a :href="version.href" btn link-decoration-none>
+          {{ version.name }}
+        </a>
+      </div>
+
     </div>
   </header>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   shortcuts: [
     [
       'btn',
-      'px-4 py-1 border rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
+      'px-4 py-1 border-none rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
     ],
     [
       'icon-btn',
